1bfa774caa42445d6db3caa612e1a9a18e4610a1
chiselko6 <alex.limontov@gmail.com>
2015-08-12 14:04:33 +0300

    Initial commit with a few references added

---
+++ Glinterion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Glinterion.Global" Language="C#" %>
---
+++ Glinterion/Scripts/angular/js/main.js
@@ -0,0 +1,6 @@
+angular.module("glinterion", [
+	"ngRoute",
+	"ngResource",
+	"glinterionControllers"
+
+]);
