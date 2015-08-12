bdf34590dcd5afeda72f5f6b1a1c8c378f985461
chiselko6 <alex.limontov@gmail.com>
2015-08-12 17:47:10 +0300

    bower_components installed, service delivery made

---
+++ Glinterion/Scripts/angular/js/controllers/HomeController.js
@@ -0,0 +1,6 @@
+﻿angular.module("glinterionControllers").controller("HomeController", HomeController);
+
+HomeController.$inject = [];
+function HomeController() {
+    
+}
---
+++ Glinterion/Scripts/angular/js/services/PhotosDeliveryService.js
@@ -0,0 +1,9 @@
+﻿angular.module("glinterionServices").service("PhotosDeliveryService", PhotosDeliveryService);
+
+PhotosDeliveryService.$inject = ["$resource"];
+
+function PhotosDeliveryService($resource) {
+    return $resource("api/photos", {}, {
+        query: { method: "GET", params: {}, isArray: true }
+    });
+}
