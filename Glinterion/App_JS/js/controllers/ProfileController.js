7083b6efdd82e6c10cde60dfda9303c5da529d48
chiselko6 <alex.limontov@gmail.com>
2015-08-14 16:35:36 +0300

    popupService has been created to divide popup matters from controller

--- Glinterion/App_JS/js/controllers/ProfileController.js
+++ Glinterion/App_JS/js/controllers/ProfileController.js
@@ -4,10 +4,10 @@ ProfileController.$inject = [
 	"$scope",
 	"$routeParams", 
 	"PhotosDeliveryService",
-	"$timeout"
+	"PhotosPopupService"
 ];
 
-function ProfileController($scope, $routeParams, PhotosDelivery, $timeout) {
+function ProfileController($scope, $routeParams, PhotosDelivery, PhotosPopupService) {
 	var profile = this;
 
 	profile.user = {};
@@ -16,6 +16,8 @@ function ProfileController($scope, $routeParams, PhotosDelivery, $timeout) {
 	profile.user.Id = $routeParams.userId;
 	profile.user.photos = PhotosDelivery.query();
 
+	// profile.galleryPhotoLinkClass = ".gallery-photo-link";
+
 	profile.user.photos.$promise.then(function(photos) {
 		var prefix = "http://" + location.host + "/";
 		// in order to set full path to files
@@ -23,52 +25,8 @@ function ProfileController($scope, $routeParams, PhotosDelivery, $timeout) {
 			photo.SrcPreview = prefix + photo.SrcPreview;
 			photo.SrcOriginal = prefix + photo.SrcOriginal;
 		});
-		$scope.$watch("photos", function (newValue, oldValue) {
-			$timeout(function() {
-				// Initialize popup as usual
-						$('.gallery-photo-link').magnificPopup({ 
-							type: 'image',
-							mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-
-							// delegate: 'a',
-
-							gallery:{
-								enabled:true,
-								preload: [0,2], // read about this option in next Lazy-loading section
-
-								navigateByImgClick: true,
-
-								arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-								tPrev: 'Previous (Left arrow key)', // title for left button
-								tNext: 'Next (Right arrow key)' // title for right button
-							},
-
-							image: {
-								cursor: 'mfp-zoom-out-cur',
-								titleSrc: "title"
-							},
-							// This will create a single gallery from all elements that have class "gallery-item"
-
-							zoom: {
-								enabled: true, // By default it's false, so don't forget to enable it
-
-								duration: 300, // duration of the effect, in milliseconds
-								easing: 'ease-in-out', // CSS transition easing function 
-
-								// // The "opener" function should return the element from which popup will be zoomed in
-								// // and to which popup will be scaled down
-								// // By defailt it looks for an image tag:
-								// opener: function(openerElement) {
-								//   // openerElement is the element on which popup was initialized, in this case its <a> tag
-								//   // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-								//   return openerElement.is('img') ? openerElement : openerElement.find('img');
-								// }
-							}
-
-						});
-			});
-		});
+		// we don't pass additional parameter as 'gallery-photo-link' to <a> because ng-class doesn't have binding to property
+		PhotosPopupService($scope, photos);
 	});
 
 	
---
+++ Glinterion/App_JS/js/services/PhotosPopupService.js
@@ -0,0 +1,57 @@
+angular.module("glinterionServices").service("PhotosPopupService", PhotosPopupService);
+
+PhotosPopupService.$inject = [
+	"$timeout"
+];
+
+function PhotosPopupService($timeout) {
+	return function($scope, photos) {
+		$scope.$watch(photos, function (newValue, oldValue) {
+			$timeout(function() {
+				// Initialize popup as usual
+				$(".gallery-photo-link").magnificPopup({ 
+					type: 'image',
+					mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+					// delegate: 'a',
+
+					gallery:{
+						enabled:true,
+						preload: [0,2], // read about this option in next Lazy-loading section
+
+						navigateByImgClick: true,
+
+						arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+						tPrev: 'Previous (Left arrow key)', // title for left button
+						tNext: 'Next (Right arrow key)' // title for right button
+					},
+
+					image: {
+						cursor: 'mfp-zoom-out-cur',
+						titleSrc: "title"
+					},
+					// This will create a single gallery from all elements that have class "gallery-item"
+
+					zoom: {
+						enabled: true, // By default it's false, so don't forget to enable it
+
+						duration: 300, // duration of the effect, in milliseconds
+						easing: 'ease-in-out', // CSS transition easing function 
+
+						// // The "opener" function should return the element from which popup will be zoomed in
+						// // and to which popup will be scaled down
+						// // By defailt it looks for an image tag:
+						// opener: function(openerElement) {
+						//   // openerElement is the element on which popup was initialized, in this case its <a> tag
+						//   // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+						//   return openerElement.is('img') ? openerElement : openerElement.find('img');
+						// }
+					}
+
+				});
+			});
+		});
+		
+	}
+}
