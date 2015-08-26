779fbe40ff76820e19168e40f380d5388ce3a51f
chiselko6 <alex.limontov@gmail.com>
2015-08-26 13:05:43 +0300

    glinterion.entities created

---
+++ Glinterion.Entities/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Glinterion.Models
+{
+    public class Account
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AccountId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public long? DurationTicks { get; set; }
+
+        [NotMapped]
+        public TimeSpan Duration { get { return TimeSpan.FromTicks(DurationTicks ?? 0); } set { DurationTicks = value.Ticks; } }
+
+        public double? MaxSize { get; set; }
+
+        // to be colored on profile page
+        public Color Color { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public virtual ICollection<AccountSerial> Serials { get; set; } 
+    }
+}
---
+++ Glinterion.Entities/Models/Photo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Glinterion.Models
+{
+    public class Photo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PhotoId { get; set; }
+
+        [Required]
+        public string SrcPreview { get; set; }
+
+        [Required]
+        public string SrcOriginal { get; set; }
+
+        public string Description { get; set; }
+
+        public double? Rating { get; set; }
+
+        [Required]
+        // in MB
+        public double Size { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
